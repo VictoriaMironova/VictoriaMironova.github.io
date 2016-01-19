3a2632e5e95a8b9d714cb38fa457a28481a395f1
VictoriaMironova <vicky.mironova@gmail.com>
2016-01-19 18:42:12 +0200

    today changes

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
  * Created by Viktoriia_Mironova on 1/19/2016.
  */
 /*------------------profile-------------------------*/
-var geninfo ='{"_id":"569e0c66c19174aa097ac038","index":0,"gender":"male","Firstname":"Fuentes","Lastname":"Woods","picture":"img/user_pic.png","status":"away","email":"fuenteswoods@qimonk.com","phone":"+1 (998) 553-2893","servLoad":73,"storageLoad":38,"bandwidth":21,"messagesNew":6,"alertsNew":1,"mailsNew":27}';
+var geninfo ='{"_id":"569e0c66c19174aa097ac038","index":0,"gender":"male","Firstname":"Fuentes","Lastname":"Woods","picture":"img/user_pic.png","status":"Online","email":"fuenteswoods@qimonk.com","phone":"+1 (998) 553-2893","servLoad":73,"storageLoad":38,"bandwidth":21,"messagesNew":6,"alertsNew":1,"mailsNew":27}';
 var genData = JSON.parse(geninfo);
 function loadProfile()
 {
@@ -31,6 +31,8 @@ function drawChart() {
         pieHole: 0.8,
         pieSliceText: 'none',
         legend: 'none',
+        backgroundColor: '#f9f9f9',
+        chartArea:{top:13,width:'85%', height:'85%'},
         slices: {
             0: { color: '#4d7496' },
             1: { color: '#eae8e8' }
